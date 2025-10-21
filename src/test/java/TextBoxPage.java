d5d823715c668b4e4b92c0c789b4a7f61ac82fbb
MataAlieva <mataalieva58@gmail.com>
2025-10-21 19:05:44 +0300

    DemoqaHw PageObject Add RegistrationPage

---
+++ src/test/java/TextBoxPage.java
@@ -0,0 +1,54 @@
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+import pages.components.RegistrationPage;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TextBoxPage {
+    private final SelenideElement header = $("h1"),
+            userNameInput = $("#userName"),
+            userEmailInput = $("#userEmail"),
+            userCurrentAddressInput = $("#currentAddress"),
+            userPermanentAddressInput = $("#permanentAddress"),
+            submitButton = $("#submit"),
+            outputField = $("#output");
+
+
+    public TextBoxPage open(String value) {
+        open(value);
+        return this;
+    }
+
+    public TextBoxPage setUserName(String value) {
+        userNameInput.setValue(value);
+        return this;
+    }
+
+
+    public TextBoxPage setUserEmail(String value) {
+        userEmailInput.setValue(value);
+        return this;
+    }
+
+    public TextBoxPage setUserCurrentAddress(String value) {
+        userCurrentAddressInput.setValue(value);
+        return this;
+    }
+
+    public TextBoxPage setUserPermanentAddress(String value) {
+        userPermanentAddressInput.setValue(value);
+        return this;
+    }
+
+    public TextBoxPage setSubmit() {
+        submitButton.click();
+        return this;
+    }
+
+    public TextBoxPage checkResult(String key, String value) {
+        outputField.$(By.id(key)).shouldHave(text(value));
+        return this;
+    }
+
+}
