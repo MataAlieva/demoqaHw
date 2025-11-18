e97840cf59653036fcaead17edb65b05f061d75a
MataAlieva <mataalieva58@gmail.com>
2025-11-18 21:49:50 +0300

    changed remote

--- src/test/java/TestBaseRemote.java
+++ src/test/java/TestBaseRemote.java
@@ -19,7 +19,7 @@ public class TestBaseRemote {
         Configuration.browserVersion = System.getProperty("browserVersion", "127.0");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.timeout = 10000;
-        Configuration.remote = System.getProperty("remote", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+        Configuration.remote = System.getProperty("remote");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.of(
